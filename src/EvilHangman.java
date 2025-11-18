6a9194927ad7547f9d511a705b26ab9e1dc915db
will-friend <wsfriend3@gmail.com>
2025-11-17 19:56:31 -0500

    Commit of updated Evil classes for game and solution. Use of inheritance for both methods. EvilSolution custom methods are work in progress (getFamily, updateWordList, Override of addGuess)

--- src/EvilHangman.java
+++ src/EvilHangman.java
@@ -1,2 +1,32 @@
-public class EvilHangman {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+
+public class EvilHangman extends Hangman {
+
+    private int solutionLength;
+    private ArrayList<String> solutionList;
+    private EvilSolution solution;
+
+    public EvilHangman() {
+        super("engDictionary.txt");
+        int maxLength = 0;
+        for (String word : wordList) {
+            maxLength = Math.max(maxLength, word.length());
+        }
+        solutionLength = new Random().nextInt(maxLength-1) + 1;
+        solutionList = new ArrayList<>();
+        for (String word : wordList) {
+            if (word.length() == solutionLength) {
+                solutionList.add(word);
+            }
+        }
+
+        solution = new EvilSolution(solutionList);
+
+    }
+
+
+
+
 }
--- src/EvilSolution.java
+++ src/EvilSolution.java
@@ -1,2 +1,55 @@
-public class EvilSolution {
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class EvilSolution extends Solution {
+
+    private ArrayList<String> wordList;
+    private int targetLength;
+
+    public EvilSolution(ArrayList<String> wordList) {
+        super();
+        this.wordList = wordList;
+        this.missingChars = this.wordList.getFirst().length();
+        this.targetLength = this.missingChars;
+        partialSolution = new ArrayList<>(missingChars);
+        for (int i = 0; i < this.missingChars; i++) {
+            partialSolution.add('_') ;
+        }
+    }
+
+    private int updateWordList(char guess) {
+        ArrayList<HashSet<String>> families = new ArrayList<HashSet<String>>();
+        HashSet<String> excludedFamily = new HashSet<>();
+        ArrayList<Integer> emptyIdx = new ArrayList<>();
+        for (int i = 0; i < targetLength; i++) {
+            if (partialSolution.get(i) == '_') {
+                emptyIdx.add(i);
+            }
+        }
+
+        for (int idx: emptyIdx) {
+            families.add(getFamily(idx, guess)) ;
+        }
+
+
+
+        return -1 ;
+    }
+
+    private HashSet<String> getFamily(int index, char guess) {
+        HashSet<String> family = new HashSet<>();
+        for (String word: wordList) {
+            if (word.charAt(index) == guess) {
+                family.add(word);
+            }
+        }
+        return family;
+    }
+
+    @Override
+    public boolean addGuess(char guess) {
+        boolean guessCorrect = false;
+        return guessCorrect;
+    }
+
 }
