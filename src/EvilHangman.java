44492a21e674b831d0392d389156067f84483680
will-friend <wsfriend3@gmail.com>
2025-11-18 10:32:03 -0500

    Moved Evil fields to local for simplicity.

--- src/EvilHangman.java
+++ src/EvilHangman.java
@@ -2,18 +2,15 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class EvilHangman extends Hangman {
-
-    private int solutionLength;
-    private ArrayList<String> solutionList;
-
+    
     public EvilHangman() {
         super("engDictionary.txt");
         int maxLength = 0;
         for (String word : wordList) {
             maxLength = Math.max(maxLength, word.length());
         }
-        solutionLength = new Random().nextInt(maxLength-1) + 1;
-        solutionList = new ArrayList<>();
+        int solutionLength = new Random().nextInt(maxLength-1) + 1;
+        ArrayList<String> solutionList = new ArrayList<>();
         for (String word : wordList) {
             if (word.length() == solutionLength) {
                 solutionList.add(word);
