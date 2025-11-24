44345de0f7eeed1a015a2873b1e6b067f6ce70c9
will-friend <wsfriend3@gmail.com>
2025-11-24 13:03:46 -0500

    Modified to account for new EvilHangman class cconstructor requiring an input argument.

--- src/EvilHangmanRunner.java
+++ src/EvilHangmanRunner.java
@@ -1,7 +1,7 @@
 public class EvilHangmanRunner {
 
     public static void main(String[] args) {
-        EvilHangman h = new EvilHangman();
+        EvilHangman h = new EvilHangman("engDictionary.txt");
         h.start();
     }
 }
